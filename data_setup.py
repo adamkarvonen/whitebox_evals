0a1179c85b31500799807ea35dac912908a948e4
Adam Karvonen <adam.karvonen@gmail.com>
2025-04-17 17:41:49 +0000

    Also download cached responses

--- data_setup.py
+++ data_setup.py
@@ -2,6 +2,7 @@ import os
 import zipfile
 import subprocess
 from pathlib import Path
+from huggingface_hub import hf_hub_download
 
 
 def setup_data():
@@ -24,6 +25,21 @@ def setup_data():
 
     print("Setup completed successfully!")
 
+def download_cached_responses():
+    repo_id = "adamkarvonen/sae_max_acts"
+    filename = "0410_data_v2.zip"
+    local_dir = "data/cached_responses"
+
+    zip_path = hf_hub_download(repo_id=repo_id, repo_type="dataset", filename=filename, local_dir=local_dir)
+
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(local_dir)
+
+    print("Unzipped cached_responses.zip")
+
+
+
 
 if __name__ == "__main__":
     setup_data()
+    download_cached_responses()

45c3ac0e1e12b175211c23b51679c40eac4ae45c
Adam Karvonen <adam.karvonen@gmail.com>
2025-05-20 20:14:22 +0000

    Increase default size

--- mypkg/pipeline/setup/dataset.py
+++ mypkg/pipeline/setup/dataset.py
@@ -73,7 +73,7 @@ def filter_by_demographics(df, gender=None, race=None, politics=None):
     return df.reset_index(drop=True)
 
 
-def load_full_anthropic_dataset(downsample_questions: int = 41):
+def load_full_anthropic_dataset(downsample_questions: int = 150):
     explicit_dataset = load_dataset("Anthropic/discrim-eval", "explicit")
     implicit_dataset = load_dataset("Anthropic/discrim-eval", "implicit")
 
